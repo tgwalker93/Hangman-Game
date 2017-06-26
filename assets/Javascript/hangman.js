0b5cbfe3f4752e4ffb5d7506ffe47510d054af4b
tgwalker93 <tgwalker93@gmail.com>
2017-06-26 03:31:41 -0700

    First push! All logic is done, just need to make it look pretty.

---
+++ assets/Javascript/hangman.js
@@ -0,0 +1,268 @@
+    //countersAndVariables!
+    var wins = 0;
+    var losses = 0;
+    var guessesRemaining = 5;
+    var words = ["sonic"];
+    var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+
+
+    
+    var computerGuess = words[Math.floor(Math.random() * words.length)];
+    
+    //document.querySelector("#underscores").innerHTML = "_ ".repeat(computerGuess.length);
+
+    //document.querySelector("#word").innerHTML = computerGuess;
+    //var x = document.querySelector("#word").innerHTML;
+    //document.querySelector("#word").style.visibility= "hidden";
+    //document.querySelector("#word").style.replaceAt(0, "s");
+
+
+
+
+    // diplays _ for all letters in word and gives them id by index
+    function displayWord() {
+    
+    correct = document.createElement('ul');
+
+    for (var i = 0; i < computerGuess.length; i++) {
+      
+      var ul = document.getElementById("word");
+      var li = document.createElement("li");
+      li.appendChild(document.createTextNode("_"));
+      li.setAttribute("id", ("letter"+i))
+      ul.appendChild(li);
+
+      }
+    }
+    //END DISPLAY WORD FUNCTION -----------------------------------------------------------
+
+    //displays new word
+    function newWord() {
+      var computerGuess = words[Math.floor(Math.random() * words.length)];
+
+      for (var i=0; i < computerGuess.length; i++) {
+          document.getElementById("letter" + i).innerHTML = "_";
+          //li.appendChild(document.createTextNode("_"));
+          //li.setAttribute("id", ("letter"+i))
+          //ul.appendChild(li);
+       
+
+      }
+
+
+    }
+
+    displayWord();
+
+
+
+    //END DISPLAY NEW WORD --------------------------------
+    
+
+
+    function startGame() {
+
+    document.onkeyup = function(event) {
+
+
+
+
+      // Determines which key was pressed.
+      var userGuess = event.key;
+
+
+      var liList = document.getElementById("listedLetters").getElementsByTagName("li");
+
+      var count = liList.length
+      var countLoops = 0;
+
+      // ADDS KEY PRESSED TO LIST AND DISPLAYS -----------------------------------------------------------------------------------------
+      for (i=0; i<count; i++) {
+          countLoops ++;
+          if (document.getElementById("guessedLetter"+i)!== null) {
+                      var getItem = document.getElementById("guessedLetter"+i).innerHTML;
+                      if (getItem===userGuess){
+                      countLoops --;
+                      }
+          }
+
+          else if (countLoops===count) {
+            var ul = document.getElementById("listedLetters");
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode(userGuess));
+            li.setAttribute("id", ("guessedLetter"+i))
+            ul.appendChild(li);
+
+          }
+
+
+      }
+
+
+
+ //     for(i=0; i<count; i++) {
+
+  //      var ul = document.getElementById("listedLetters");
+   //     var li = document.createElement("li");
+   //     li.appendChild(document.createTextNode(userGuess));
+  //      li.setAttribute("id", ("guessedLetter"+i))
+   //     ul.appendChild(li);
+
+
+ //     }
+
+
+
+      // ********************************************************************************************
+
+
+
+
+
+      //Function that takes a string parameter and sees if userGuess is in that string
+      function checkList(word) {
+
+      //var ul = document.getElementById("word");
+      //var items = ul.getElementsByTagName("li");
+
+        var wrongCounter = 0;
+        //FOR LOOP TO ITERATE THROUGH WORD AND CHECK IF LETTER IS THE LETTER THAT THE USER TYPED ----------------------
+        for(i=0; i<word.length; i++) {
+
+              if (word[i] === userGuess) {
+                    var li = document.getElementById(("letter"+i)).innerHTML=word[i];
+    
+
+
+                  //CHECK IF PLAYER WON STARTS HERE-------------------------------------------------------------
+                    var saveWord = "";
+
+        
+
+
+
+                  //for loop to check player's progress on word
+                  for (var i = 0; i < computerGuess.length; ++i) {
+                    var letter = document.getElementById(("letter"+i)).innerHTML;
+                    var saveWord = saveWord.concat(letter);
+                  }
+
+
+
+
+
+
+                  //check's if player already won ------------------------
+                  if (saveWord===word){
+                    var youWin = "YOU WON THIS GAME! Press Enter to play again!"
+                    document.querySelector("#game").innerHTML = youWin;
+                    wins ++;
+                  //UPDATES WIN AND LOSS COUNTER -----------------------------
+                  guessesRemaining = 5; 
+                  var winCounter = "Wins: " + wins + "<br />" + 
+                                    "Losses: " + losses + "<br />" +
+                                    "Guesses Remaining: " + guessesRemaining;           
+                    document.querySelector("#winsAndLosses").innerHTML = winCounter;
+                    //****************************************************************  
+                    var gameover = "YOU WON THE GAME! PRESS ENTER TO PLAY AGAIN!";
+                    document.querySelector("#game").innerHTML = gameover;
+                    //IF USER PRESS ENTER AFTER WINNING ----------------------------------------------------------
+
+                        document.onkeyup = function(event) {
+                           if(event.which == 13 || event.keyCode == 13) { 
+
+                                restartGame();
+                                startGame();
+
+
+
+                        }
+
+
+
+                      }
+
+
+                      }
+            //CHECK IF PLAYER WON ENDS HERE ------------------------------------------------------
+              
+                 }
+            else {
+              wrongCounter ++;
+            }
+            if (wrongCounter===word.length) {
+              guessesRemaining --;
+              //UPDATES WIN AND LOSS COUNTER -----------------------------
+              var guessCounter = "Wins: " + wins + "<br />" + 
+                                "Losses: " + losses + "<br />" +
+                                "Guesses Remaining: " + guessesRemaining;           
+              document.querySelector("#winsAndLosses").innerHTML = guessCounter;
+              //****************************************************************  
+              var gameWrong = "You Guessed Wrong! You have " + guessesRemaining + " guesses remaing!";
+              document.querySelector("#game").innerHTML = gameWrong;
+              if (guessesRemaining===0) {
+                  var gameover = "Game Over! Try Again. Press Enter to play again!";
+                  document.querySelector("#game").innerHTML = gameover;
+                  losses ++;
+                  //UPDATES WIN AND LOSS COUNTER -----------------------------
+                  guessesRemaining = 5; 
+                  var lossCounter = "Wins: " + wins + "<br />" + 
+                                    "Losses: " + losses + "<br />" +
+                                    "Guesses Remaining: " + guessesRemaining;
+                  document.querySelector("#winsAndLosses").innerHTML = lossCounter;
+                  //****************************************************************
+
+
+                   //PLAYER CAN PRESS ENTER TO PLAY AGAIN! ---------------------
+                  document.onkeyup = function(event) {
+                           if(event.which == 13 || event.keyCode == 13) { 
+
+                                restartGame();
+                                startGame();
+
+
+                              }
+                            }
+
+
+
+                  //*************************************************************
+
+                }
+            }
+          }
+
+
+      //else {
+      //guessesRemaining--;
+      //var guess = "You guessed wrong! Number of guesses remaining: " + guessesRemaining; 
+      //document.querySelector("#game").innerHTML = guess;
+      //return null;
+      //  }   
+    }
+
+    checkList(computerGuess);
+    //Function to restart the game! 
+    function restartGame() {
+
+      var computerGuess = words[Math.floor(Math.random() * words.length)];
+      guessesRemaining = 5;
+      newWord();
+      document.getElementById("listedLetters").innerHTML = "<li id='guess'> </li>";
+      var gameStart = "Type any letter to make a guess!";
+      document.querySelector("#game").innerHTML = gameStart;
+
+
+    }
+
+
+  }
+
+
+
+
+}
+
+startGame();
+
