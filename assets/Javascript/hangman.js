5519f2d1d45a88745e336328b6c45b342f29ef59
tgwalker93 <tgwalker93@gmail.com>
2017-06-28 04:08:40 -0700

    Made it look nice! Two bugs that I need help with: How to change songs when player plays a new game, and how to not decrement guessesRemaining after play reselects a character that he already lost a guess for.

--- assets/Javascript/hangman.js
+++ assets/Javascript/hangman.js
@@ -2,13 +2,30 @@
     var wins = 0;
     var losses = 0;
     var guessesRemaining = 5;
-    var words = ["sonic", "mario", "link", "pikachu", "fox"];
+    var words = ["sonic", "kirby"];
     var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var alreadyGuessed = false;
+    var guessesAlreadyMade = [];
 
+
+    function checkIfArrayIsUnique(arr) {
+    var map = {}, i, size;
+
+    for (i = 0, size = arr.length; i < size; i++){
+        if (map[arr[i]]){
+            return false;
+        }
+
+        map[arr[i]] = true;
+    }
+
+    return true;
+}
 
 
     
     var computerGuess = words[Math.floor(Math.random() * words.length)];
+
     
     //document.querySelector("#underscores").innerHTML = "_ ".repeat(computerGuess.length);
 
@@ -20,17 +37,299 @@
 
 
 
+
+
+
+    // OBJECTS FOR WORDS *************************************************************************************************
+
+
+    //SONIC
+    var sonic = {
+
+
+        getMusic: function() {
+
+          var audio = document.getElementById("audiotag");
+          var song = document.createElement("source");
+          
+          song.setAttribute('src', 'assets/audio/sonic.mp3');
+          audio.appendChild(song);
+        
+      },
+
+        getImage: function() {
+          document.getElementById("mysteryCharacter").src = "assets/images/sonic.png";
+
+        }
+
+
+
+
+
+    };
+    //END SONIC
+    
+
+    //MARIO
+    var mario = {
+
+
+        getMusic: function() {
+
+          var audio = document.getElementById("audiotag");
+          var song = document.createElement("source");
+          
+          song.setAttribute('src', 'assets/audio/mario.mp3');
+          audio.appendChild(song);
+             },
+
+
+          getImage: function() {
+          document.getElementById("mysteryCharacter").src = "assets/images/mario.png";
+
+        }
+
+
+
+    };
+     //END MARIO
+
+
+
+     //donkeykong
+    var donkeykong = {
+
+
+        getMusic: function() {
+
+          var audio = document.getElementById("audiotag");
+          var song = document.createElement("source");
+          
+          song.setAttribute('src', 'assets/audio/donkeykong.mp3');
+          audio.appendChild(song);
+        
+      },
+
+        getImage: function() {
+          document.getElementById("mysteryCharacter").src = "assets/images/donkeykong.png";
+
+        }
+
+
+
+
+
+    };
+    //END DONKEY KONG
+
+    
+    //FOX
+    var fox = {
+
+
+        getMusic: function() {
+
+          var audio = document.getElementById("audiotag");
+          var song = document.createElement("source");
+          
+          song.setAttribute('src', 'assets/audio/fox.mp3');
+          audio.appendChild(song);
+        
+      },
+
+        getImage: function() {
+          document.getElementById("mysteryCharacter").src = "assets/images/fox.jpg";
+
+        }
+
+
+
+
+
+    };
+    //END FOX
+
+
+
+    //KIRBY
+    var kirby = {
+
+
+        getMusic: function() {
+
+          var audio = document.getElementById("audiotag");
+          var song = document.createElement("source");
+          song.setAttribute('src', 'assets/audio/kirby.mp3');
+          audio.appendChild(song);
+        
+      },
+
+        getImage: function() {
+          document.getElementById("mysteryCharacter").src = "assets/images/kirby.png";
+
+        }
+
+
+
+
+
+    };
+    //END KIRBY
+
+
+    //LINK
+    var link = {
+
+
+        getMusic: function() {
+
+          var audio = document.getElementById("audiotag");
+          var song = document.createElement("source");
+          
+          song.setAttribute('src', 'assets/audio/link.mp3');
+          audio.appendChild(song);
+        
+      },
+
+        getImage: function() {
+          document.getElementById("mysteryCharacter").src = "assets/images/link.png";
+
+        }
+
+
+
+
+
+    };
+    //END link
+
+
+    //MEGA MAN 
+    var megaman = {
+
+
+        getMusic: function() {
+
+          var audio = document.getElementById("audiotag");
+          var song = document.createElement("source");
+          
+          song.setAttribute('src', 'assets/audio/megaman.mp3');
+          audio.appendChild(song);
+        
+      },
+
+        getImage: function() {
+          document.getElementById("mysteryCharacter").src = "assets/images/megaman.png";
+
+        }
+
+
+
+
+
+    };
+    //END MEGA MAN
+
+
+    //pacman
+    var pacman = {
+
+
+        getMusic: function() {
+
+          var audio = document.getElementById("audiotag");
+          var song = document.createElement("source");
+          
+          song.setAttribute('src', 'assets/audio/pacman.mp3');
+          audio.appendChild(song);
+        
+      },
+
+        getImage: function() {
+          document.getElementById("mysteryCharacter").src = "assets/images/pacman.png";
+
+        }
+
+
+
+
+
+    };
+    //END PACMAN
+
+
+    //PIKACHU
+    var pikachu = {
+
+
+        getMusic: function() {
+
+          var audio = document.getElementById("audiotag");
+          var song = document.createElement("source");
+          
+          song.setAttribute('src', 'assets/audio/pikachu.mp3');
+          audio.appendChild(song);
+        
+      },
+
+        getImage: function() {
+          document.getElementById("mysteryCharacter").src = "assets/images/pikachu.png";
+
+        }
+
+
+
+
+
+    };
+    //END PIKACHU
+
+
+    //SPYRO
+    var spyro = {
+
+
+        getMusic: function() {
+
+          var audio = document.getElementById("audiotag");
+          var song = document.createElement("source");
+          
+          song.setAttribute('src', 'assets/audio/spryo.mp3');
+          audio.appendChild(song);
+        
+      },
+
+        getImage: function() {
+          document.getElementById("mysteryCharacter").src = "assets/images/spryo.png";
+
+        }
+
+
+
+
+
+    };
+    //END SPYRO
+
+
+
+    
+
+
+
+    //END WORD OBJECTS ***************************************************************************************************
+    eval(computerGuess).getMusic();
     // diplays _ for all letters in word and gives them id by index
     function displayWord() {
-    
-    correct = document.createElement('ul');
+
+    //correct = document.createElement('ul');
 
     for (var i = 0; i < computerGuess.length; i++) {
 
       var ul = document.getElementById("word");
       var li = document.createElement("li");
       li.appendChild(document.createTextNode("_"));
-      li.setAttribute("id", ("letter"+i))
+      li.setAttribute("id", ("letter"+i));
       ul.appendChild(li);
 
       }
@@ -61,9 +360,11 @@
      //  ul.appendChild(li);
   // *********************************************************************************************
 //	  }
-      computerGuess = words[Math.floor(Math.random() * words.length)];
+
+      //computerGuess = words[Math.floor(Math.random() * words.length)];
       displayWord();
       document.getElementById("listedLetters").innerHTML = "<li id='guess'> </li>";
+
 
 
 
@@ -79,14 +380,16 @@
 
     function startGame() {
 
-    document.onkeyup = function(event) {
 
+    document.onkeyup = function(event) {
 
 
 
       // Determines which key was pressed.
       var userGuess = event.key;
 
+
+      //guessesAlreadyMade.push(userGuess);
       
 
  
@@ -155,28 +458,22 @@
 
 
 
-
-
       //Function that takes a string parameter and sees if userGuess is in that string
       function checkList(word) {
 
       //var ul = document.getElementById("word");
       //var items = ul.getElementsByTagName("li");
-
         var wrongCounter = 0;
         //FOR LOOP TO ITERATE THROUGH WORD AND CHECK IF LETTER IS THE LETTER THAT THE USER TYPED ----------------------
         for(i=0; i<word.length; i++) {
 
               if (word[i] === userGuess) {
 
-
-              		//Changes _ to correct letter guessed
+              		//Changes _ to correct letter(s) guessed
               		for (i=0; i<word.length; i++) {
               			if(word[i] === userGuess) {
-              			var li = document.getElementById(("letter"+i)).innerHTML=word[i];
-
+              			var li = document.getElementById(("letter"+i)).innerHTML=word[i];       
               			}
-
 
               		}
      
@@ -215,6 +512,9 @@
                     //****************************************************************  
                     var gameover = "YOU WON THE GAME! PRESS ENTER TO PLAY AGAIN!";
                     document.querySelector("#game").innerHTML = gameover;
+                    eval(computerGuess).getImage();
+
+                   // guessesAlreadyMade = [];
                     //IF USER PRESS ENTER AFTER WINNING ----------------------------------------------------------
 
                         document.onkeyup = function(event) {
@@ -239,16 +539,42 @@
             else {
               wrongCounter ++;
             }
-            if (wrongCounter===word.length) {
+            if (wrongCounter===word.length) { 
+
+              
 
 
+
+
+              //FOR LOOP TEST TO CHECK ARRAY FOR ALREADY GUESSED 
+                if(checkIfArrayIsUnique(guessesAlreadyMade)===false){
+                    alreadyGuessed = true;
+                  }
+
+
+              //DECREMENTING GUESSES REMAING COUNTER IF GUESS IS WRONG
             	var pat = /^[a-z]+$/;
-	  			if(pat.test(userGuess)){
+	  			    if(pat.test(userGuess)){
 
-	  			guessesRemaining --;
-	  				}
 
-           
+
+                      if(alreadyGuessed===false) {
+
+                        guessesRemaining --;
+
+                      }
+
+              
+                      
+
+
+
+                      }
+                guessesRemaing = false; 
+                  
+
+            
+
               //UPDATES WIN AND LOSS COUNTER -----------------------------
               var guessCounter = "Wins: " + wins + "<br />" + 
                                 "Losses: " + losses + "<br />" +
@@ -269,8 +595,8 @@
                   document.querySelector("#winsAndLosses").innerHTML = lossCounter;
                   //****************************************************************
 
-
                    //PLAYER CAN PRESS ENTER TO PLAY AGAIN! ---------------------
+                   //guessesAlreadyMade = [];
                   document.onkeyup = function(event) {
                            if(event.which == 13 || event.keyCode == 13) { 
 
@@ -306,8 +632,8 @@
 
     //Function to restart the game! 
     function restartGame() {
-
-      var computerGuess = words[Math.floor(Math.random() * words.length)];
+      computerGuess = words[Math.floor(Math.random() * words.length)];
+      eval(computerGuess).getMusic();
       guessesRemaining = 5;
       newWord();
       document.getElementById("listedLetters").innerHTML = "<li id='guess'> </li>";
